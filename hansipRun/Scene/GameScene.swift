e16b6ff75ca48c7d48b028918953c1f028afde89
Fandrian Rhamadiansyah <fandrian.rhamadiansyah@gmail.com>
2020-06-12 16:19:55 +0700

    add poskamling

--- hansipRun/Scene/GameScene.swift
+++ hansipRun/Scene/GameScene.swift
@@ -9,32 +9,141 @@
 import SpriteKit
 import GameplayKit
 
-struct PhysicsCategory {
-    static let hansip: UInt32 = 0b1 // 1
-    static let land: UInt32 = 0b10 // 2
 
-}
-
-class GameScene: SKScene, SKPhysicsContactDelegate {
+class GameScene: SKScene {
     
-    var gameState = State.preGame
-    var startLabel : SKLabelNode?
-    var hansip : SKSpriteNode?
+    var hansip : SKSpriteNode!
     
     override func didMove(to view: SKView) {
         physicsWorld.contactDelegate = self
-        self.physicsBody = SKPhysicsBody(edgeLoopFrom: CGRect(x: -1792, y: -1792, width: 1792*2, height: 1792*2))
-
+        self.physicsBody = SKPhysicsBody(edgeLoopFrom: CGRect(x: -self.size.width/2, y: -self.size.height/2, width: self.size.width, height: self.size.height))
+        self.physicsBody?.categoryBitMask = PhysicsCategory.boundary
+        self.physicsBody?.contactTestBitMask = PhysicsCategory.hansip
+        
         createBackground()
         createLand()
         createHansip()
+        setupSpawnAction()
+        setupFinish()
     }
     
-    //update
+    
     override func update(_ currentTime: TimeInterval) {
-
-//        moveBackground()
+        moveBackground()
     }
+    
+    
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        hansip.run(SKAction.applyImpulse(CGVector(dx: 0.0, dy: 500.0), duration: 0.1))
+        hansip.removeAction(forKey: "movingAnimation")
+        hansip.texture = SKTexture(imageNamed: "Hansip - Jump-1.png")
+        
+    }
+    
+    func createHansip() {
+        hansip = SKSpriteNode(imageNamed: "Hansip - Stand-1.png")
+        
+        hansip.name = "hansip"
+        hansip.size = CGSize(width: (self.scene?.size.width)! * 0.15, height: (self.scene?.size.width)! * 0.15)
+        hansip.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        
+        hansip.position = CGPoint(x: frame.midX, y: frame.midY)
+        hansip.zPosition = 2
+        
+        self.addChild(hansip)
+        
+        //add physicsbody
+        let hansipBody = SKPhysicsBody(rectangleOf: CGSize(width: hansip.size.width, height: hansip.size.height))
+        hansipBody.isDynamic = true
+        hansipBody.affectedByGravity = true
+        hansipBody.allowsRotation = false
+        hansipBody.categoryBitMask = PhysicsCategory.hansip
+        hansipBody.contactTestBitMask = PhysicsCategory.land | PhysicsCategory.boundary
+        hansip.physicsBody = hansipBody
+        
+        
+        // add texture
+        hansipRunningAnimation(asset: hansip)
+    }
+    
+    func setupSpawnAction() {
+        let spawnBgMeteorAction = SKAction.run {
+            self.createObstacle()
+        }
+        
+        let waitAction = SKAction.wait(forDuration: 1)
+        
+        let sequence = SKAction.sequence([spawnBgMeteorAction, waitAction])
+        run(SKAction.repeatForever(sequence), withKey: "spawnObstacle")
+
+    }
+    
+    
+}
+
+//MARK: - Physics Contact
+
+extension GameScene : SKPhysicsContactDelegate {
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        let bitMask = contact.bodyA.categoryBitMask | contact.bodyB.categoryBitMask
+        
+        if (bitMask == PhysicsCategory.land | PhysicsCategory.hansip) {
+            let hansip = (contact.bodyA.node?.name == "hansip" ? contact.bodyA.node : contact.bodyB.node) as! SKSpriteNode
+            
+            hansipRunningAnimation(asset: hansip)
+            
+        } else if (bitMask == PhysicsCategory.boundary | PhysicsCategory.hansip) {
+            print("lose")
+            if let scene = SKScene(fileNamed: "MainMenuScene") {
+                scene.scaleMode = scaleMode
+                view?.presentScene(scene)
+            }
+        } else if (bitMask == PhysicsCategory.poskamling | PhysicsCategory.hansip) {
+            print("finish")
+            if let scene = SKScene(fileNamed: "MainMenuScene") {
+                scene.scaleMode = scaleMode
+                view?.presentScene(scene)
+            }
+        }
+        
+    }
+    
+    
+}
+
+//MARK: - Obstacle Function
+
+extension GameScene {
+    
+    func createObstacle() {
+            let obstacleArray = ["challenge-log", "challenge-rocks"]
+            let randomInt = Int.random(in: 0...1)
+            let obstacle = SKSpriteNode(imageNamed: obstacleArray[randomInt])
+            obstacle.anchorPoint = CGPoint(x: 0, y: 0)
+            obstacle.setScale(0.1 * CGFloat.random(in: 5...20))
+            obstacle.position = CGPoint(x: frame.maxX - obstacle.size.width, y: frame.minY + (self.scene?.size.height)!/4)
+            obstacle.zPosition = 3
+            self.addChild(obstacle)
+            
+            print("ada")
+            
+    //        let obstacleBody = SKPhysicsBody(rectangleOf: CGSize(width: obstacle.size.width, height: obstacle.size.height))
+            let obstacleBody = SKPhysicsBody(texture: SKTexture(imageNamed: obstacleArray[randomInt]), alphaThreshold: 0, size: obstacle.size)
+            obstacleBody.isDynamic = false
+            obstacle.physicsBody = obstacleBody
+
+            let moveAction = SKAction.moveTo(x: frame.minX, duration: 2)
+            obstacle.run(SKAction.sequence([moveAction, SKAction.removeFromParent()]))
+        }
+    
+}
+
+//MARK: - Background and Movement
+
+extension GameScene {
     
     func createBackground() {
         for i in 0...3 {
@@ -60,63 +169,76 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         land.physicsBody?.isDynamic = false
         land.physicsBody?.affectedByGravity = true
         land.physicsBody?.categoryBitMask = PhysicsCategory.land
+        land.physicsBody?.contactTestBitMask = PhysicsCategory.hansip
         
         self.addChild(land)
     }
     
-    func createHansip() {
-        hansip = SKSpriteNode(imageNamed: "Hansip - Stand-1.png")
-
-        hansip!.name = "hansip"
-        hansip!.size = CGSize(width: (self.scene?.size.width)! * 0.15, height: (self.scene?.size.width)! * 0.15)
-        hansip!.anchorPoint = CGPoint(x: 0.5, y: 0.5)
-
-        hansip!.position = CGPoint(x: frame.midX, y: frame.midY)
-        hansip!.zPosition = 2
+    
+    func hansipRunningAnimation(asset : SKSpriteNode) {
         
-        self.addChild(hansip!)
-        
-        //add physicsbody
-        let hansipBody = SKPhysicsBody(rectangleOf: CGSize(width: hansip!.size.width, height: hansip!.size.height))
-        hansipBody.isDynamic = true
-        hansipBody.affectedByGravity = true
-        hansipBody.categoryBitMask = PhysicsCategory.hansip
-        hansip!.physicsBody = hansipBody
-//        hansip.physicsBody = SKPhysicsBody(edgeLoopFrom: CGRect(x: frame.midX, y: frame.midY, width: (self.scene?.size.width)! * 0.15, height: (self.scene?.size.width)! * 0.15))
-
-        
-        // add texture
         var moving : [SKTexture] = []
-
-        for i in 1...4 {
-            moving.append(SKTexture(imageNamed: "Hansip - Stand-\(i).png"))
+        
+        for i in 1...6 {
+            moving.append(SKTexture(imageNamed: "Hansip - walk V3-\(i).png"))
         }
-        let movingAnimation = SKAction.animate(with: moving, timePerFrame: 0.1)
-
-        hansip?.run(SKAction.repeatForever(movingAnimation))
+        let movingAnimation = SKAction.animate(with: moving, timePerFrame: 1/12)
+        
+        asset.run(SKAction.repeatForever(movingAnimation), withKey: "movingAnimation")
     }
     
     func moveBackground() {
         self.enumerateChildNodes(withName: "sky") { (node, error) in
             node.position.x -= 3
-
+            
             if node.position.x < -((self.scene?.size.width)!) {
                 node.position.x += (self.scene?.size.width)! * 3
             }
         }
     }
     
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        guard let touch = touches.first else { return }
+}
 
-        if let node = self.nodes(at: touch.location(in: self)).first as? SKSpriteNode {
-            if node == hansip {
-                hansip!.run(SKAction.applyForce(CGVector(dx: 0.0, dy: 5000.0), duration: 0.1))
-                print("jump")
-            }
-        }
+
+//MARK: - Finish Category
+
+extension GameScene {
+    
+    func spawnPoskamling() {
+        let poskamling = SKSpriteNode(imageNamed: "poskamling")
+        poskamling.anchorPoint = CGPoint(x: 0, y: 0)
+        poskamling.setScale(2)
+        poskamling.position = CGPoint(x: frame.maxX - poskamling.size.width, y: frame.minY + (self.scene?.size.height)!/4)
+        poskamling.zPosition = 4
+        self.addChild(poskamling)
+        print("muncul")
+        
+        poskamling.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: poskamling.size.width/2, height: poskamling.size.height))
+        poskamling.physicsBody?.isDynamic = false
+        
+        
+        poskamling.physicsBody?.categoryBitMask = PhysicsCategory.poskamling
+        poskamling.physicsBody?.contactTestBitMask = PhysicsCategory.hansip
+        
+        let moveAction = SKAction.moveTo(x: frame.minX, duration: 2)
+        poskamling.run(SKAction.sequence([moveAction]))
+        
     }
-
-
+    
+    func setupFinish() {
+        let spawnPoskamling = SKAction.run {
+            self.spawnPoskamling()
+        }
+        
+        let levelDuration = SKAction.wait(forDuration: 10)
+        let waitAction = SKAction.wait(forDuration: 5)
+        let removeAction = SKAction.run {
+            self.removeAction(forKey: "spawnObstacle")
+        }
+        let sequence = SKAction.sequence([levelDuration, removeAction, waitAction, spawnPoskamling])
+        run(sequence, withKey: "spawnPoskamling")
+    }
+    
+    
 }
 
---
+++ hansipRun/Scene/PhysicsCategory.swift
@@ -0,0 +1,18 @@
+//
+//  PhysicsCategory.swift
+//  hansipRun
+//
+//  Created by Fandrian Rhamadiansyah on 12/06/20.
+//  Copyright © 2020 Fandrian Rhamadiansyah. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+struct PhysicsCategory {
+    static let boundary : UInt32 = 0b1
+    static let hansip: UInt32 = 0b10 // 2
+    static let land: UInt32 = 0b100 // 4
+    static let poskamling : UInt32 = 0b1000
+    
+}
