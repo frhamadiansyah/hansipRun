b90668fddc4249a744c90f84eb46fd7561cda785
Fandrian Rhamadiansyah <fandrian.rhamadiansyah@gmail.com>
2020-06-15 15:19:51 +0700

    fix animation

--- hansipRun/Scene/GameOverScene.swift
+++ hansipRun/Scene/GameOverScene.swift
@@ -11,6 +11,10 @@ import SpriteKit
 
 class GameOverScene: SKScene {
     
+    // character
+    var hansip : SKSpriteNode!
+    var ground : SKSpriteNode!
+    
     var youDeadLabel : SKLabelNode!
     var tapToRetryLabel : SKLabelNode!
     
@@ -18,13 +22,15 @@ class GameOverScene: SKScene {
         createBackground()
         addYouDeadLabel()
         addRetryLabel()
+        createGround()
+        createHansip()
     }
     
     func addYouDeadLabel() {
         youDeadLabel = SKLabelNode(text: "You are DEAD")
         youDeadLabel?.name = "youDeadLabel"
         youDeadLabel?.fontSize = 50
-        youDeadLabel?.position = CGPoint(x: 0, y: 0)
+        youDeadLabel?.position = CGPoint(x: 0, y: self.frame.height/8)
         youDeadLabel?.zPosition = 1
         self.addChild(youDeadLabel!)
     }
@@ -33,7 +39,7 @@ class GameOverScene: SKScene {
         tapToRetryLabel = SKLabelNode(text: "Tap anywhere to retry")
         tapToRetryLabel?.name = "tapToRetryLabel"
         tapToRetryLabel?.fontSize = 40
-        tapToRetryLabel?.position = CGPoint(x: 0, y: -self.frame.height/5)
+        tapToRetryLabel?.position = CGPoint(x: 0, y: 0)
         tapToRetryLabel?.zPosition = 2
         print("retry")
         self.addChild(tapToRetryLabel!)
@@ -51,6 +57,34 @@ class GameOverScene: SKScene {
         
     }
     
+    func createHansip() {
+        hansip = SKSpriteNode(imageNamed: "Hansip - Stand-1.png")
+        hansip.name = "hansip"
+        hansip.setScale(3)
+        hansip.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        
+        hansip.position = CGPoint(x: frame.midX, y: frame.minY + ground.size.height/2 + hansip.size.height/2)
+        hansip.zPosition = 2
+        
+        self.addChild(hansip)
+    }
+    
+    func createGround() {
+        ground = SKSpriteNode(imageNamed: "foreground-land")
+        ground.name = "land"
+        ground.size = CGSize(width: (self.scene?.size.width)!, height: (self.scene?.size.height)!/2)
+        ground.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        ground.position = CGPoint(x: frame.midX, y: frame.minY)
+        ground.zPosition = 1
+        ground.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: ground.size.width, height: ground.size.height))
+        ground.physicsBody?.isDynamic = false
+        ground.physicsBody?.affectedByGravity = true
+        ground.physicsBody?.categoryBitMask = PhysicsCategory.land
+        ground.physicsBody?.contactTestBitMask = PhysicsCategory.hansip
+        
+        self.addChild(ground)
+    }
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         
         if let scene = SKScene(fileNamed: "GameScene") {
@@ -58,6 +92,10 @@ class GameOverScene: SKScene {
             view?.presentScene(scene)
         }
         
-        
+    }
+    
+    func animateScene() {
+        let moveAction = SKAction.moveTo(x: frame.midX, duration: TimeInterval(1))
+        hansip.run(moveAction)
     }
 }
